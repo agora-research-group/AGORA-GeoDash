5d013375eb81762157d79b3884a36d90d19db084
Thiago Henrique Poiani <thpoiani@gmail.com>
2016-05-04 02:06:23 -0300

    Tabs to spaces

--- src/main/java/br/usp/icmc/agora/GeodashApplication.java
+++ src/main/java/br/usp/icmc/agora/GeodashApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GeodashApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeodashApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GeodashApplication.class, args);
+    }
 }
