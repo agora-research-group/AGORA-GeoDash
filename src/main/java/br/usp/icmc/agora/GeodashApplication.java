a09ea12a10fc4a960399df51da8c2c73b93057dd
Thiago Henrique Poiani <thpoiani@gmail.com>
2016-05-04 01:38:24 -0300

    Initial commit with Spring Initializr

---
+++ src/main/java/br/usp/icmc/agora/GeodashApplication.java
@@ -0,0 +1,12 @@
+package br.usp.icmc.agora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeodashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeodashApplication.class, args);
+	}
+}
---
+++ src/test/java/br/usp/icmc/agora/GeodashApplicationTests.java
@@ -0,0 +1,18 @@
+package br.usp.icmc.agora;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GeodashApplication.class)
+@WebAppConfiguration
+public class GeodashApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
